3ebc3c1ff95388138b197fd63515c55681acf75b
Dane Harrigan <dane.harrigan@gmail.com>
2014-05-21 19:20:30 -0700

    fix typo

--- view.php
+++ view.php
@@ -3,4 +3,4 @@
 $file_name = "/tmp/" . uniqid() . ".php";
 stream_copy_to_stream(fopen("php://input", "rb"), $handle = fopen($file_name, "wb"));
 fclose($handle);
-echo shell_exec("hhvm --tempfile $file_name");
+echo shell_exec("hhvm --temp-file $file_name");
