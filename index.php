2b514d2714c198a338e8c483da514e848cdf42a5
Dane Harrigan <dane.harrigan@gmail.com>
2014-05-21 19:18:44 -0700

    use two ace editors

--- index.php
+++ index.php
@@ -9,80 +9,73 @@ body {
   background: #fafafa;
 }
 
-#results, button {
-  font-family: "Helvetica Neue", Helvetica, Arial, "lucida grande", sans-serif;
-  color: #4e5665;
-}
-
 section {
   position: fixed;
   top: 20px;
   bottom: 80px;
+  left: 20px;
   width: 48%;
   border: 1px solid #eee;
   background: #fff;
 }
 
-#code {
-  left: 20px;
-}
-
-#source {
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-}
-
-#results {
+section:last-child {
+  left: auto;
   right: 20px;
 }
 
-#results div {
-  position: relative;
-  padding: 10px;
+section > div {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  
 }
 
-#code button {
+button {
+  font: bold 16px "Helvetica Neue", Helvetica, Arial, "lucida grande", sans-serif;
+  color: #fff;
+  background: #fd8724;
   position: absolute;
   bottom: -70px;
   border: none;
-  background: #fd8724;
-  color: #fff;
   padding: 20px;
-  font-size: 16px;
-  font-weight: bold;
+}
+
+.ace_gutter {
+  background: transparent !important;
 }
 </style>
 
 <script src="http://ajaxorg.github.io/ace-builds/src/ace.js"></script>
 <script>
 document.addEventListener("DOMContentLoaded", function(){
-  var editor = ace.edit("source");
-  editor.getSession().setUseWorker(false);
-  editor.setTheme("ace/theme/github");
+  var source = ace.edit("source");
+  source.getSession().setUseWorker(false);
+  source.setTheme("ace/theme/github");
+
+  var result = ace.edit("result");
+  result.getSession().setUseWorker(false);
+  result.setTheme("ace/theme/github");
 
   var view = document.querySelector("button")
   view.addEventListener("click", function(e){
     e.preventDefault()
     var req = new XMLHttpRequest()
     req.addEventListener("load", function(response){
-      var result = document.querySelector("#results > div")
-      result.innerHTML = response.currentTarget.response
+      result.setValue(response.currentTarget.response, 1)
     })
 
     req.open("POST", "/view", true)
-    req.send(editor.getValue())
+    req.send(source.getValue())
   })
 })
 </script>
 <main>
-  <section id="code">
+  <section>
     <div id="source"></div>
     <button type="submit">Compile</button>
   </section>
-  <section id="results">
-    <div></div>
+  <section>
+    <div id="result"></div>
   </section>
 </main>
